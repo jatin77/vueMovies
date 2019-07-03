41019ed2511ec16407a0b6edb066bf28759ebe3f
Acid <clockstopfountain@gmail.com>
2019-07-03 19:30:47 +0530

    Updates

---
+++ precache-manifest.b422ae6a8902c785293e89c6358ec89f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463bd439f8106d810273d69971b60398",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "adc40a8ebc40d850807f",
+    "url": "/my-app/static/css/main.70f3e793.chunk.css"
+  },
+  {
+    "revision": "23250ef1970c9567f9ce",
+    "url": "/my-app/static/js/2.11201880.chunk.js"
+  },
+  {
+    "revision": "adc40a8ebc40d850807f",
+    "url": "/my-app/static/js/main.b2a528f7.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "4c14021095fd20edc900e2e9b91d318e",
+    "url": "/my-app/static/media/1_CsJ05WEGfunYMLGfsT2sXA.4c140210.gif"
+  },
+  {
+    "revision": "e0a9ddfee325ea07149c2cfc175aec48",
+    "url": "/my-app/static/media/adult-black-and-white-bokeh-1993689.e0a9ddfe.jpg"
+  },
+  {
+    "revision": "d137d2a515884f94d8c7000b1299a175",
+    "url": "/my-app/static/media/advertise-advertisement-advertising-1534518.d137d2a5.jpg"
+  },
+  {
+    "revision": "85c323a4bb89dbce77c187fdeca59dd0",
+    "url": "/my-app/static/media/album-black-classic-167092_burned.85c323a4.png"
+  },
+  {
+    "revision": "0cc0242f3b3ab590751f517384a76ef6",
+    "url": "/my-app/static/media/audio-cassette-cassette-tape-1626481.0cc0242f.jpg"
+  },
+  {
+    "revision": "6ab95eedd4e1f5d84ffe1e2eb186dcd9",
+    "url": "/my-app/static/media/celeb.6ab95eed.jpg"
+  },
+  {
+    "revision": "badb11d1ed931506f2fc69bbe3c741ba",
+    "url": "/my-app/static/media/charlies.badb11d1.jpg"
+  }
+]);
